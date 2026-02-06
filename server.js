8ef6dd4ac1e19d15d0f4ab7ea20027b0e0004733
RUANMODSZ <luanriquetto@gmail.com>
2026-02-06 15:12:11 -0300

    server.js

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
 
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// servir a pasta public
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("Servidor rodando na porta " + PORT);
+});
