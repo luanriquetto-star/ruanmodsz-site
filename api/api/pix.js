4c4386e827e7b949c8d85bab9986c1bbb31c633e
RUANMODSZ <luanriquetto@gmail.com>
2026-02-06 15:43:25 -0300

    Delete api/api/pix.js

--- api/api/pix.js
+++
@@ -1,8 +0,0 @@
-export default function handler(req, res) {
-  res.status(200).json({
-    chave_pix: "adbe80ca-0439-4b2e-8968-eeb8d8381270",
-    valor: "0.10",
-    nome: "RUAN MODSZ",
-    descricao: "Pedido teste R$0,10"
-  });
-}
